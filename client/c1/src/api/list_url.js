4129d09d188b7af6ced44cb41deffd9afe332317
keelim <kimh00335@gmail.com>
2021-11-04 14:08:01 +0900

    [ADD] introduce API interface for services

---
+++ client/c1/src/api/list_url.js
@@ -0,0 +1,4 @@
+export const listUrl = {
+  baeURL: 'http://k5b201.p.ssafy.io:4000',
+  release_url: 'https://naver.com',
+};
--- client/c1/src/api/mainApi.js
+++ client/c1/src/api/mainApi.js
@@ -1,12 +1,24 @@
 import axios from 'axios';
+import {listUrl} from './list_url';
 
-axios.defaults.baseURL = 'http://k5b201.p.ssafy.io:4000';
+axios.defaults.baseURL = listUrl.baeURL;
 
 export default {
   getServerStatus() {
-    return axios
-      .get('/user')
-      .then(res => res)
-      .catch(error => error);
+    return axios.get('/user');
+  },
+
+  getRelease() {
+    console.log(this);
+    try {
+      const data = axios({
+        url: listUrl.release_url,
+        datatype: 'json',
+        method: 'get',
+      });
+      return data;
+    } catch (e) {
+      return e;
+    }
   },
 };
