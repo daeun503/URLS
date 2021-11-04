8ab9892acc84db0d810e2814a733802d50c656b6
keelim <kimh00335@gmail.com>
2021-11-05 01:07:46 +0900

    [UPDATE] update mainApi.js and introduce mixin

--- client/c1/src/api/mainApi.js
+++ client/c1/src/api/mainApi.js
@@ -1,24 +1,33 @@
 import axios from 'axios';
-import {listUrl} from './list_url';
+import {listUrl, samples} from './list_url';
 
 axios.defaults.baseURL = listUrl.baeURL;
 
 export default {
   getServerStatus() {
-    return axios.get('/user');
+    return axios
+      .get('/user')
+      .then(res => res)
+      .catch(error => error);
   },
 
   getRelease() {
-    console.log(this);
-    try {
-      const data = axios({
-        url: listUrl.release_url,
-        datatype: 'json',
-        method: 'get',
-      });
-      return data;
-    } catch (e) {
-      return e;
-    }
+    // return axios
+    //   .get(listUrl.release_url)
+    //   .then(res => res)
+    //   .catch(error => error);
+
+    // TODO
+    return samples;
+  },
+  inject(url, token) {
+    // TODO
+    return axios
+      .post('/inject', {
+        url,
+        token,
+      })
+      .then(res => res)
+      .catch(error => error);
   },
 };
