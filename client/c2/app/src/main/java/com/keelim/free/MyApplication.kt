7109a9be6137125ed5593e140dc7f738570cd064
keelim <kimh00335@gmail.com>
2021-11-15 02:00:44 +0900

    [ADD] ADD Flipper monitoring libraries

--- client/c2/app/src/main/java/com/keelim/free/MyApplication.kt
+++ client/c2/app/src/main/java/com/keelim/free/MyApplication.kt
@@ -1,6 +1,12 @@
 package com.keelim.free
 
 import android.app.Application
+import com.facebook.flipper.android.AndroidFlipperClient
+import com.facebook.flipper.android.utils.FlipperUtils
+import com.facebook.flipper.core.FlipperClient
+import com.facebook.flipper.plugins.inspector.DescriptorMapping
+import com.facebook.flipper.plugins.inspector.InspectorFlipperPlugin
+import com.facebook.soloader.SoLoader
 import com.google.android.material.color.DynamicColors
 import com.keelim.free.notification.NotificationChannels
 import com.keelim.free.util.ComponentLogger
@@ -23,5 +29,13 @@ class MyApplication : Application() {
         NotificationChannels.init(this)
         componentLogger.initialize(this)
         DynamicColors.applyToActivitiesIfAvailable(this)
+
+        SoLoader.init(this, false)
+
+        if (BuildConfig.DEBUG && FlipperUtils.shouldEnableFlipper(this)) {
+            val client: FlipperClient = AndroidFlipperClient.getInstance(this)
+            client.addPlugin(InspectorFlipperPlugin(this, DescriptorMapping.withDefaults()))
+            client.start()
+        }
     }
 }
